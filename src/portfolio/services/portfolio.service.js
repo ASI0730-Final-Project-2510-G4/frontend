c0a13ef6d345a954c67df2834972a27bf512ca6b
SoyValzzz <valentino2805@gmail.com>
2025-06-21 15:06:59 -0500

    feat: enhance portfolio component to conditionally display project actions for owners and update project fetching logic

--- src/portfolio/services/portfolio.service.js
+++ src/portfolio/services/portfolio.service.js
@@ -1,12 +1,24 @@
-import axios from 'axios';
+/*import axios from 'axios';
 
 export async function getProjects(profileId) {
     try {
-        const response = await axios.get('https://my-json-server.typicode.com/SoyValzzz/Creatilink-db2/projects');
+        const response = await axios.get(`https://creatilink-api-production-4e2f.up.railway.app/api/v1/projects/by-profile/${profileId}`);
 
         const profileProjects = response.data.filter(project => project.profileId === profileId);
 
         return profileProjects;
+    } catch (error) {
+        console.error('Error al obtener proyectos:', error);
+        return [];
+    }
+}*/
+
+import axios from 'axios';
+
+export async function getProjects(profileId) {
+    try {
+        const response = await axios.get(`http://localhost:3000/projects?profileId=${profileId}`);
+        return response.data;
     } catch (error) {
         console.error('Error al obtener proyectos:', error);
         return [];
